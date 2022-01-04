3dce039a17a883eb82385b69df2f5054830f4a79
Patricia Gonzalez <aloha@bondcoder.tech>
2022-01-04 21:31:31 +0000

    Create Practicas.sol

---
+++ Practicas.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.8.3;
+
+import "contracts/Base.sol";
+
+contract Practicas is Base{
+    function devolverSueldoBase() public override view returns(int) {
+        return 0;
+    }
+}
